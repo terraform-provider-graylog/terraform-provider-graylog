10b0ada6d669b3bef5a745b4e94b92beb934dd03
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-04-28 19:30:08 +0900

    feat: add Pipeline resource

---
+++ graylog/resource/system/pipeline/pipeline/delete.go
@@ -0,0 +1,21 @@
+package pipeline
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/suzuki-shunsuke/terraform-provider-graylog/graylog/client"
+)
+
+func destroy(d *schema.ResourceData, m interface{}) error {
+	ctx := context.Background()
+	cl, err := client.New(m)
+	if err != nil {
+		return err
+	}
+	if _, err := cl.Pipeline.Delete(ctx, d.Id()); err != nil {
+		return fmt.Errorf("failed to delete a pipeline %s: %w", d.Id(), err)
+	}
+	return nil
+}
---
+++ graylog/resource/system/pipeline/pipeline/update.go
@@ -0,0 +1,25 @@
+package pipeline
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/suzuki-shunsuke/terraform-provider-graylog/graylog/client"
+)
+
+func update(d *schema.ResourceData, m interface{}) error {
+	ctx := context.Background()
+	cl, err := client.New(m)
+	if err != nil {
+		return err
+	}
+	data, err := getDataFromResourceData(d)
+	if err != nil {
+		return err
+	}
+	if _, _, err := cl.Pipeline.Update(ctx, d.Id(), data); err != nil {
+		return fmt.Errorf("failed to update a pipeline %s: %w", d.Id(), err)
+	}
+	return nil
+}
