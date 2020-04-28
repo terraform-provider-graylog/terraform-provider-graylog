c1a7f4af88b9708cbaeb3a9989b4b92223585522
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-04-28 19:22:20 +0900

    feat: add pipeline rule client

---
+++ graylog/client/system/pipeline/rule/client.go
@@ -0,0 +1,78 @@
+package rule
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/suzuki-shunsuke/go-httpclient/httpclient"
+)
+
+type Client struct {
+	Client *httpclient.Client
+}
+
+func (cl *Client) Get(
+	ctx context.Context, id string,
+) (map[string]interface{}, *http.Response, error) {
+	if id == "" {
+		return nil, nil, errors.New("id is required")
+	}
+
+	body := map[string]interface{}{}
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method:       "GET",
+		Path:         "/system/pipelines/rule/" + id,
+		ResponseBody: &body,
+	})
+	return body, resp, err
+}
+
+func (cl *Client) Create(
+	ctx context.Context, data map[string]interface{},
+) (map[string]interface{}, *http.Response, error) {
+	if data == nil {
+		return nil, nil, errors.New("request body is nil")
+	}
+
+	body := map[string]interface{}{}
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method:       "POST",
+		Path:         "/system/pipelines/rule",
+		RequestBody:  data,
+		ResponseBody: &body,
+	})
+	return body, resp, err
+}
+
+func (cl *Client) Update(
+	ctx context.Context, id string, data map[string]interface{},
+) (map[string]interface{}, *http.Response, error) {
+	if id == "" {
+		return nil, nil, errors.New("id is required")
+	}
+	if data == nil {
+		return nil, nil, errors.New("request body is nil")
+	}
+
+	body := map[string]interface{}{}
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method:       "PUT",
+		Path:         "/system/pipelines/rule/" + id,
+		RequestBody:  data,
+		ResponseBody: &body,
+	})
+	return body, resp, err
+}
+
+func (cl *Client) Delete(ctx context.Context, id string) (*http.Response, error) {
+	if id == "" {
+		return nil, errors.New("id is required")
+	}
+
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method: "DELETE",
+		Path:   "/system/pipelines/rule/" + id,
+	})
+	return resp, err
+}
