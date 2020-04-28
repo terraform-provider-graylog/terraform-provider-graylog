857f5cdab9931ad70fc474f2eae1f28a297ffa20
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-04-28 19:20:19 +0900

    feat: add Extractor client

---
+++ graylog/client/system/input/extractor/client.go
@@ -0,0 +1,56 @@
+package extractor
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/suzuki-shunsuke/go-httpclient/httpclient"
+)
+
+type Client struct {
+	Client *httpclient.Client
+}
+
+func (cl *Client) Get(ctx context.Context, inputID, id string) (map[string]interface{}, *http.Response, error) {
+	body := map[string]interface{}{}
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method:       "GET",
+		Path:         "/system/inputs/" + inputID + "/extractors/" + id,
+		ResponseBody: &body,
+	})
+	return body, resp, err
+}
+
+func (cl *Client) Create(
+	ctx context.Context, inputID string, data map[string]interface{},
+) (map[string]interface{}, *http.Response, error) {
+	body := map[string]interface{}{}
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method:       "POST",
+		Path:         "/system/inputs/" + inputID + "/extractors",
+		RequestBody:  data,
+		ResponseBody: &body,
+	})
+	return body, resp, err
+}
+
+func (cl *Client) Update(
+	ctx context.Context, inputID, id string, data map[string]interface{},
+) (map[string]interface{}, *http.Response, error) {
+	body := map[string]interface{}{}
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method:       "PUT",
+		Path:         "/system/inputs/" + inputID + "/extractors/" + id,
+		RequestBody:  data,
+		ResponseBody: &body,
+	})
+	return body, resp, err
+}
+
+func (cl *Client) Delete(ctx context.Context, inputID, id string) (*http.Response, error) {
+	resp, err := cl.Client.Call(ctx, &httpclient.CallParams{
+		Method: "DELETE",
+		Path:   "/system/inputs/" + inputID + "/extractors/" + id,
+	})
+	return resp, err
+}
