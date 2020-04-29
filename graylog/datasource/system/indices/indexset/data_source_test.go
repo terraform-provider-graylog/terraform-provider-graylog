53c887190f6ae560086dfaf338dc6cade088229c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-04-29 09:20:33 +0900

    test: add test

--- graylog/datasource/system/indices/indexset/data_source_test.go
+++ graylog/datasource/system/indices/indexset/data_source_test.go
@@ -131,6 +131,95 @@ data "graylog_index_set" "test" {
 		),
 	}
 
+	readStepByPrefix := resource.TestStep{
+		ResourceName: "data.graylog_index_set.test",
+		PreConfig: func() {
+			testutil.SetHTTPClient(t, getRoute)
+		},
+		Config: `
+data "graylog_index_set" "test" {
+  index_prefix = "graylog"
+}
+`,
+		Check: resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr("data.graylog_index_set.test", "title", "Default index set"),
+			resource.TestCheckResourceAttr("data.graylog_index_set.test", "description", "The Graylog default index set"),
+		),
+	}
+
+	resource.Test(t, resource.TestCase{
+		Providers: testutil.SingleDataSourceProviders("graylog_index_set", DataSource()),
+		Steps: []resource.TestStep{
+			readStep,
+			readStepByPrefix,
+		},
+	})
+}
+
+func TestAccIndexSet_byIndexSetID(t *testing.T) {
+	if err := testutil.SetEnv(); err != nil {
+		t.Fatal(err)
+	}
+
+	getRoute := flute.Route{
+		Name: "get indexset",
+		Matcher: &flute.Matcher{
+			Method: "GET",
+		},
+		Tester: &flute.Tester{
+			Path:         "/api/system/indices/index_sets/5ea81cc02ab79c00129dbf1c",
+			PartOfHeader: testutil.Header(),
+		},
+		Response: &flute.Response{
+			Response: func(req *http.Request) (*http.Response, error) {
+				return &http.Response{
+					StatusCode: 200,
+					Body: ioutil.NopCloser(strings.NewReader(`{
+  "id": "5ea81cc02ab79c00129dbf1c",
+  "title": "Graylog Events",
+  "description": "Stores Graylog events.",
+  "index_prefix": "gl-events",
+  "shards": 4,
+  "replicas": 0,
+  "rotation_strategy_class": "org.graylog2.indexer.rotation.strategies.TimeBasedRotationStrategy",
+  "rotation_strategy": {
+    "type": "org.graylog2.indexer.rotation.strategies.TimeBasedRotationStrategyConfig",
+    "rotation_period": "P1M"
+  },
+  "retention_strategy_class": "org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy",
+  "retention_strategy": {
+    "type": "org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig",
+    "max_number_of_indices": 12
+  },
+  "creation_date": "2020-04-28T12:08:32.18Z",
+  "index_analyzer": "standard",
+  "index_optimization_max_num_segments": 1,
+  "index_optimization_disabled": false,
+  "field_type_refresh_interval": 60000,
+  "writable": true,
+  "default": false
+}`)),
+				}, nil
+			},
+		},
+	}
+
+	readStep := resource.TestStep{
+		ResourceName: "data.graylog_index_set.test",
+		PreConfig: func() {
+			testutil.SetHTTPClient(t, getRoute)
+		},
+		Config: `
+data "graylog_index_set" "test" {
+  index_set_id = "5ea81cc02ab79c00129dbf1c"
+}
+`,
+		Check: resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr("data.graylog_index_set.test", "title", "Graylog Events"),
+			resource.TestCheckResourceAttr("data.graylog_index_set.test", "description", "Stores Graylog events."),
+		),
+	}
+
 	resource.Test(t, resource.TestCase{
 		Providers: testutil.SingleDataSourceProviders("graylog_index_set", DataSource()),
 		Steps: []resource.TestStep{
