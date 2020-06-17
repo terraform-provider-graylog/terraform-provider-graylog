5e2f21a3fa3b5cbae76ad113074c4f03418815a0
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-06-17 20:53:20 +0900

    refactor: refactor Data Source Dashboard

--- graylog/datasource/dashboard/util.go
+++ graylog/datasource/dashboard/util.go
@@ -7,8 +7,16 @@ import (
 	"github.com/terraform-provider-graylog/terraform-provider-graylog/graylog/util"
 )
 
+const (
+	keyID          = "id"
+	keyDashboardID = "dashboard_id"
+	keyDashboards  = "dashboards"
+	keyViews       = "views"
+	keyTitle       = "title"
+)
+
 func setDataToResourceData(d *schema.ResourceData, data map[string]interface{}) error {
-	id, ok := util.RenameKey(data, "id", "dashboard_id")
+	id, ok := util.RenameKey(data, keyID, keyDashboardID)
 
 	if err := convert.SetResourceData(d, dashboard.Resource(), data); err != nil {
 		return err
