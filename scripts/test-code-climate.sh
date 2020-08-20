165688ff0b6402111957dfb785c574735e329a2f
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-20 09:04:18 +0900

    ci: replace CodeCov to CodeClimate

---
+++ scripts/test-code-climate.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+ee() {
+  echo "+ $*"
+  eval "$@"
+}
+
+cd "$(dirname "$0")/.."
+
+repo_name=${1:-}
+if [ -z "$repo_name" ]; then
+  echo "the repository name is required" >&2
+  exit 1
+fi
+
+ee curl -L -o /usr/local/bin/cc-test-reporter https://codeclimate.com/downloads/test-reporter/test-reporter-0.6.3-linux-amd64
+ee chmod a+x /usr/local/bin/cc-test-reporter
+
+ee cc-test-reporter before-build
+
+ee mkdir -p .code-climate
+
+for d in $(go list ./...); do
+  echo "$d"
+  profile=.code-climate/$d/profile.txt
+  coverage=.code-climate/$d/coverage.json
+  ee mkdir -p "$(dirname "$profile")" "$(dirname "$coverage")"
+  ee go test -race -coverprofile="$profile" -covermode=atomic "$d"
+  if [ "$(wc -l < "$profile")" -eq 1 ]; then
+    continue
+  fi
+  ee cc-test-reporter format-coverage -t gocov -p "github.com/${repo_name}" -o "$coverage" "$profile"
+done
+
+result=.code-climate/codeclimate.total.json
+# shellcheck disable=SC2046
+ee cc-test-reporter sum-coverage $(find .code-climate -name coverage.json) -o "$result"
+ee cc-test-reporter upload-coverage -i "$result"
